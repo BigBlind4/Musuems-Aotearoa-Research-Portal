328780482afcce896913fd5422668038f645d2ea
Rayna <fengyali@ecs.vuw.ac.nz>
2017-08-21 09:58:20 +1200

    add unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,22 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { AppComponent } from './app.component';
 
 describe('App', () => {
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]})
+      .compileComponents();
+
+      fixture = TestBed.createComponent(AppComponent);
+      comp = fixture.componentInstance;
   });
 
-  it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+  it ('should create AppComponent', () => {
+    expect(comp).toBeDefined();//toBe(true, 'should create AppComponent');
   });
 });
