748a217e06b9eee9304332363dc4b5d10dd589d9
Ori <kraemezoe@ecs.vuw.ac.nz>
2017-10-02 13:18:30 +1300

    renamed 'post' as 'reply' and 'by' as 'author' for clarity, added body attribute to table topics -- reviewed by hana

--- draftCode/draftMessageBoard/database.php
+++ draftCode/draftMessageBoard/database.php
@@ -19,26 +19,27 @@ PRIMARY KEY (cat_id)
 
 CREATE TABLE topics (
 topic_id        INT(8) NOT NULL AUTO_INCREMENT,
-topic_subject       VARCHAR(255) NOT NULL,
+topic_title       VARCHAR(255) NOT NULL,
 topic_date      DATETIME NOT NULL,
-topic_cat       INT(8) NOT NULL,
-topic_by        INT(8) NOT NULL,
+topic_cat      INT(8) NOT NULL, --may not need
+topic_body      TEXT NOT NULL,
+topic_author        INT(8) NOT NULL,
 PRIMARY KEY (topic_id)
 ) TYPE=INNODB;
 
-CREATE TABLE posts (
-post_id         INT(8) NOT NULL AUTO_INCREMENT,
-post_content        TEXT NOT NULL,
-post_date       DATETIME NOT NULL,
-post_topic      INT(8) NOT NULL,
-post_by     INT(8) NOT NULL,
-PRIMARY KEY (post_id)
+CREATE TABLE replies (
+reply_id         INT(8) NOT NULL AUTO_INCREMENT,
+reply_content        TEXT NOT NULL,
+reply_date       DATETIME NOT NULL,
+reply_topic      INT(8) NOT NULL,
+reply_author     INT(8) NOT NULL,
+PRIMARY KEY (reply_id)
 ) TYPE=INNODB;
 
 ALTER TABLE topics ADD FOREIGN KEY(topic_cat) REFERENCES categories(cat_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE topics ADD FOREIGN KEY(topic_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE topics ADD FOREIGN KEY(topic_author) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
 
-ALTER TABLE posts ADD FOREIGN KEY(post_topic) REFERENCES topics(topic_id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE replies ADD FOREIGN KEY(reply_topic) REFERENCES topics(topic_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE posts ADD FOREIGN KEY(post_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE replies ADD FOREIGN KEY(reply_author) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
